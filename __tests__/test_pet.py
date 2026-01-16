e44142104f54026a90d02db602425e7d94be8240
elainecoelhopereira <elainecoelho2018@gmail.com>
2026-01-16 14:04:15 -0300

    first commit

---
+++ __tests__/test_pet.py
@@ -0,0 +1,50 @@
+# 1 - bibliotecas
+import pytest                    # engine / framwork de teste de unidade
+import requests                  # framework de test de API
+import json
+
+# 2 - classe (Opcional no Python, em muitos casos)
+
+# 2.1 - atributos ou variaveis
+# consulta e resultado esperado
+pet_id = 918286801               # codigo do animal
+pet_name = "Snoopy"              # nome do animal
+pet_category_id = 1              # codigo da categoria do animal
+pet_category_name = "dog"        # titulo da categoria
+pet_tag_id = 1                   # codigo do rotulo
+pet_tag_name = "vacinado"        # titulo do rotulo
+pet_status = "available"         # status do animal
+
+# informações em comum
+url = 'https://petstore.swagger.io/v2/pet'          # endereço
+headers = { 'Content-Type': 'application/json' }    # formato dos dados trafegados
+
+# 2.2 - funções / método
+
+def test_post_pet():
+    # configura
+    # dados de entrega estão no arquivo json
+    pet=open('./fixtures/json/pet1.json')        # abre o arquivo json
+    data=json.loads(pet.read())                  # ler o conteudo e carrega como json em uma variavel data
+    # dados de saíde / resultado esperado estão nos atributos acima das funções
+
+    # executa 
+    response = requests.post(           # executa o método post c as informaçõe a segui
+        url=url,                        # endereço
+        headers=headers,                # cabeçalho / informações extras da mensagem
+        data=json.dumps(data),          # a mensagem = json
+        timeout=5                       # tempo limite da transição, em segundos
+    )
+
+    # valida
+    response_body = response.json()        # cria uma variavel e carrega a resposta em formato json
+
+    assert response.status_code == 200
+    assert response_body['id'] == pet_id
+    assert response_body['name'] == pet_name
+    assert response_body['category']['name'] == pet_category_name
+    assert response_body['tags'][0]['name'] == pet_tag_name
+
+
+
+
