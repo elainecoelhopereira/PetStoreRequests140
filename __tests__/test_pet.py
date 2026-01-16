2eedaea2f45d7624dd3bb1d4ca2d6114166608d3
elainecoelhopereira <elainecoelho2018@gmail.com>
2026-01-16 16:26:26 -0300

    Adicionado Put e Delete.

--- __tests__/test_pet.py
+++ __tests__/test_pet.py
@@ -13,7 +13,6 @@ pet_category_id = 1              # codigo da categoria do animal
 pet_category_name = "dog"        # titulo da categoria
 pet_tag_id = 1                   # codigo do rotulo
 pet_tag_name = "vacinado"        # titulo do rotulo
-pet_status = "available"         # status do animal
 
 # informações em comum
 url = 'https://petstore.swagger.io/v2/pet'          # endereço
@@ -64,5 +63,52 @@ def test_get_pet():
     assert response_body['name'] == pet_name
     assert response_body['category']['id'] == pet_category_id
     assert response_body['tags'][0]['id'] == pet_tag_id
-    assert response_body['status'] == pet_status
+    assert response_body['status'] == 'available'
 
+
+def test_put_pet():
+    # Configura
+    # dados de entrada vem de um arquivo json
+    pet = open('./fixtures/json/pet2.json')
+    data = json.loads(pet.read())
+    # dados de saída / resultado esperado vem dos atributos descritos antes das funções
+
+    # Executa
+    response = requests.put(
+        url=url,  
+        headers=headers,
+        data=json.dumps(data),
+        timeout=5
+    )
+
+       
+    # Valida
+    response_body = response.json()
+
+    assert response.status_code == 200
+    assert response_body['id'] == pet_id
+    assert response_body['name'] == pet_name    
+    assert response_body['category']['id'] == pet_category_id
+    assert response_body['category']['name'] == pet_category_name
+    assert response_body['tags'][0]['id'] == pet_tag_id
+    assert response_body['tags'][0]['name'] == pet_tag_name
+    assert response_body['status'] == 'sold'
+
+def test_delete_pet():
+    # Configura
+    # dados de entrada vem de um arquivo json
+    
+    # Executa
+    response = requests.delete(
+        url=f'{url}/{pet_id}',  
+        headers=headers        
+    )
+       
+    # Valida
+    response_body = response.json()
+
+    assert response.status_code == 200   # status code 200 é so p saber se chegou
+    assert response_body['code'] == 200   # status code 200 é p saber se chegou certo
+    assert response_body['type'] == 'unknown'
+    assert response_body['message'] == str(pet_id)    
+   
